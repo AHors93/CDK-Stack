54c309a9fa359b6017a19d82b4f223b2f27eb510
Adam Horscraft <adam.horscraft@foodhub.co.uk>
2021-11-24 10:13:46 +0000

    pipeline deployed

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core'
 import * as codecommit from '@aws-cdk/aws-codecommit'
+import {CodeBuildStep, CodePipepline, CodePipelineSource} from '@aws-cdk/piplelines'
 
 export class HelloPipelineStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +11,24 @@ export class HelloPipelineStack extends cdk.Stack {
          */
         new codecommit.Repository(this, 'FirstCDKRepo', {
             repositoryName: "FirstCDKRepo"
+        })
+
+        /**
+         * Pipeline declaration
+         */
+        const pipeline = new CodePipepline(this, 'Pipeline', {
+            pipelineName: 'FirstCDKPipeline',
+            synth: new CodeBuildStep('SynthStep', {
+                input: CodePipelineSource.codeCommit(repo, 'master'),
+                installCommands: [
+                    'yarn add -g aws-cdk'
+                ],
+                commands: [
+                    'yarn ci',
+                    'yarn run build',
+                    'yarn cdk synth'
+                ]
+            })
         })
     }
 }
